95786c5c4f5cfbc5fb9f93f6eff459714d6ed7dc
Scott Weinstein <sweinstein27@gmail.com>
2018-05-07 18:14:49 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,18 +1,18 @@
 # Make your shoe class here!
 
-class shoe
-
-
-def initialize(brand)
-  @brand = brand
-end
-
-
-
-
-
-
-
-
-
-end
+# class shoe
+# 
+# 
+# def initialize(brand)
+#   @brand = brand
+# end
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# end
